b9a771b25a5104a741f95346ebe4308e1735a1e9
Oreder <stepup.ored@gmail.com>
2018-10-17 17:50:27 +0300

    try to using django_heroku for deployment

--- myweb/settings.py
+++ myweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,13 +26,9 @@ SECRET_KEY = 'wi7q8r2*0bi5+led9qw6-j51)nh=wz(sa+-21t0efcdi@@ua&9'
 # SECURITY WARNING: don't run with debug turned on in production!
 import dj_database_url
 
-ENVIRONMENT = 'production'
-
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
-DATABASES['default'] = dj_database_url.config(default='DATABASE_URL_HERE')
 
 
 # Application definition
@@ -134,3 +131,6 @@ STATICFILES_DIRS = [
 ]
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
