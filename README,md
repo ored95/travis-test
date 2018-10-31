f497a070be4d118e20dfad819806faa4e7fa1341
Oreder <stepup.ored@gmail.com>
2018-10-31 20:12:37 +0300

    Final version worked successfully without bugs. Added readme content

---
+++ README,md
@@ -0,0 +1,22 @@
+# travis-test
+[![Build Status](https://travis-ci.org/Oreder/travis-test.svg?branch=master)](https://travis-ci.org/Oreder/travis-test) [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy) 
+A project-based approach to test Web development with the Django framework and how to deploy app by Heroku
+
+
+## Content
+Check 3 numbers can be a Pythagore's triplet or not?
+
+## Installation
+With pip in tow, we’ll install the basic following dependencies:
++ Django-Brotli
++ Gunicorn
+
+To do so, simple run the following commands in the terminal:
+```sh
+pip install -r requirements.txt
+```
+
+## Build apps
++ Front-end based on: Bootstrap 4
++ Continous integration: travis-ci
++ Deployed by: heroku. 
--- myweb/views.py
+++ myweb/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    triplet = []
+    try:
+        result = ""
+        triplet = ""
+    
+        if request.method == 'POST':
+            triplet = '(' + request.POST.get('x-value') + ', ' + request.POST.get('y-value') + ', ' + request.POST.get('z-value') + ')'
+            x = float(request.POST.get('x-value'))
+            y = float(request.POST.get('y-value'))
+            z = float(request.POST.get('z-value'))
+        
 
-    if request.method == 'POST':
-        triplet.append(request.POST.get('x-value'))
-        triplet.append(request.POST.get('y-value'))
-        triplet.append(request.POST.get('z-value'))
+            array = [abs(x), abs(y), abs(z)]
+            array.sort()
+            err = array[0] ** 2 + array[1] ** 2 - array[2] ** 2
+            extendCaption = "A "
+            if abs(err) > 1E-16:
+                extendCaption += "non-"
+            result = extendCaption + "Pythagore's triplet"
 
-    return render(request, 'index.html', { 'triplet': triplet })
+        return render(request, 'index.html', { 'triplet': triplet, 'result': result })
+    except:
+        return HttpResponse(status=404)
